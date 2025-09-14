5cef878caf25f043fc335b6f116a6f4740d3ffff
Bocheng-Feng <bochengfeng.phy@stu.pku.edu.cn>
2025-09-15 01:50:40 +0800

    clean script

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ from sym_plot import basic_plot, ppsd_plot
 
 ########################### user config ################################
 # List of simulation suites to analyze, representing different host halo mass scales
-#suite_names = ["SymphonyGroup", "SymphonyCluster"]
+suite_names = ["SymphonyLMC","SymphonyMilkyWay","SymphonyGroup", "SymphonyLCluster", "SymphonyCluster"]
 
 # Path to the root directory containing Symphony simulation data (particle and halo catalogs)
-#simul_dir = '/Volumes/Carol/Symphony'
+simul_dir = '/media/31TB4/bocheng/symphony'
 
 # Target redshift at which the analysis is performed
-redshifts = [0.05, 0.1 , 5, 9]
+redshifts = [0, 0.01, 0.1 , 0.5, 0.7, 1, 2, 3, 5]
 
 # Radial binning parameters for profile measurements:
 # n_bins = number of radial bins
@@ -41,8 +41,7 @@ if __name__ == "__main__":
     for redshift in redshifts:
         print(f'------processing z={redshift}-------')
         # Loop over each simulation suite to process data and produce measurements
-        for suite in ["SymphonyGroup", "SymphonyCluster"]:
-            simul_dir = '/Volumes/Carol/Symphony'
+        for suite in suite_names:
             sim_dir = symlib.get_host_directory(simul_dir, suite, 0)  # Get directory for suite host halo 0
             scale = symlib.scale_factors(sim_dir)                      # Array of scale factors for snapshots
             z = 1/scale - 1                                            # Convert scale factors to redshifts
@@ -52,42 +51,23 @@ if __name__ == "__main__":
             closest_redshift = z[idx]                # Actual redshift of the chosen snapshot
             snap = closest_snapshot                  # Rename for clarity in downstream code
 
-            data_dir = f'output/z_{redshift}/data'  # Directory to save measurement data
+            data_dir = f'/home/bocheng/Projects/Symphony-PPSD/output/z_{redshift}/data'  # Directory to save measurement data
             os.makedirs(data_dir, exist_ok=True)    # Create directory if it doesn't exist
 
-            basic_measure.density_velocity_mass(simul_dir, data_dir, suite, snap, n_bins, r_min, r_max)
-            ppsd_measure.ppsd_profiles(data_dir, suite)
-            ppsd_measure.smooth_ppsd_slopes(data_dir, suite, method='constant_jerk')
-            halo_properties.process_halo_properties(simul_dir, data_dir, suite, snap)
-
-        for suite in ["SymphonyLMC", "SymphonyMilkyWay", "SymphonyLCluster"]:
-            simul_dir = '/Volumes/Atlas/Symphony'
-            sim_dir = symlib.get_host_directory(simul_dir, suite, 0)  # Get directory for suite host halo 0
-            scale = symlib.scale_factors(sim_dir)                      # Array of scale factors for snapshots
-            z = 1/scale - 1                                            # Convert scale factors to redshifts
-
-            idx = np.argmin(np.abs(z - redshift))    # Find index of snapshot closest to target redshift
-            closest_snapshot = idx                   # Snapshot ID corresponding to closest redshift
-            closest_redshift = z[idx]                # Actual redshift of the chosen snapshot
-            snap = closest_snapshot                  # Rename for clarity in downstream code
-
-            data_dir = f'output/z_{redshift}/data'  # Directory to save measurement data
-            os.makedirs(data_dir, exist_ok=True)    # Create directory if it doesn't exist
-
-            basic_measure.density_velocity_mass(simul_dir, data_dir, suite, snap, n_bins, r_min, r_max)
+            basic_measure.density_velocity_mass(simul_dir, data_dir, suite, snap, n_bins=40, r_min=1e-3 if redshift==0 else 1e-2, r_max=1.5)
             ppsd_measure.ppsd_profiles(data_dir, suite)
             ppsd_measure.smooth_ppsd_slopes(data_dir, suite, method='constant_jerk')
             halo_properties.process_halo_properties(simul_dir, data_dir, suite, snap)
 
 ################################ plot ###################################
         # Create directory for saving figures corresponding to the target redshift
-        fig_dir = f'output/z_{redshift}/figure'
+        fig_dir = f'/home/bocheng/Projects/Symphony-PPSD/output/z_{redshift}/figure'
         os.makedirs(fig_dir, exist_ok=True)
 
-        base_dir = f'output/z_{redshift}'
-
-        suite_names=["SymphonyLMC", "SymphonyMilkyWay", "SymphonyGroup", "SymphonyLCluster", "SymphonyCluster"]
+        base_dir = f'/home/bocheng/Projects/Symphony-PPSD/output/z_{redshift}'
         basic_plot.plot_density_velocity(base_dir, suite_names, einasto_fit=False, mask_range=[1e-2,1], plot_range=[1e-2,1])
         basic_plot.plot_anisotropy(base_dir, suite_names, mask_range=[1e-2,1], plot_range=[1e-2,1])
         ppsd_plot.plot_normalized_ppsd(base_dir, suite_names, plot_range=[1e-2,1])
         ppsd_plot.plot_ppsd_slope(base_dir, suite_names, plot_range=[1e-2,1])
+        print(f'------z={redshift} completed-------')
+
